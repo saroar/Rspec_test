2441b88c9319f9ed29cf92bf74350c8367a9f112
Saroar <saroar9@gmail.com>
2015-12-03 01:42:47 +0300

    Setup Bootstrap

---
+++ spec/features/create_article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'Creating Articles' do
+  scenario 'A user creates a new article' do
+    visit '/'
+
+    click_link 'New Article'
+
+    file_in 'Title', with: 'Creating first article'
+    fill_in 'Body', with: 'Hello welcome to ruby test world'
+    click_button 'Create Article'
+
+    expect(page).to have_contect('Article has beed created')
+    expect(page.current_path).to eq(articles_path)
+  end
+end
