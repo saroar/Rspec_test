af2e4fc20d38ae0a05d989c4b0da6c4c58cfd6de
Saroar <saroar9@gmail.com>
2015-12-05 15:16:26 +0300

    create article sucessfully

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -6,11 +6,11 @@ RSpec.feature 'Creating Articles' do
 
     click_link 'New Article'
 
-    file_in 'Title', with: 'Creating first article'
+    fill_in 'Title', with: 'Creating first article'
     fill_in 'Body', with: 'Hello welcome to ruby test world'
     click_button 'Create Article'
 
-    expect(page).to have_contect('Article has beed created')
+    expect(page).to have_content('Article has beed created')
     expect(page.current_path).to eq(articles_path)
   end
 end
