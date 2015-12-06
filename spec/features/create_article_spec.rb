39dea52c1ef3df033e23a3e42fe291af4126ac47
Saroar <saroar9@gmail.com>
2015-12-06 22:02:35 +0300

    Add validation to Article

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -13,4 +13,18 @@ RSpec.feature 'Creating Articles' do
     expect(page).to have_content('Article has beed created')
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario 'article validation' do
+    visit '/'
+
+    click_link 'New Article'
+
+    fill_in "Title", with: ''
+    fill_in 'Body', with: ''
+    click_button 'Create Article'
+
+    expect(page).to have_content("Article has not beed created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
