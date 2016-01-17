442b7290a2c3bd23e28706df52dd41f7dc8d3bc1
Saroar <saroar9@gmail.com>
2016-01-17 13:45:08 +0300

    restrect  new edit and delete options for signed in users and owners

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -2,7 +2,43 @@ require 'rails_helper'
 
 RSpec.feature 'Show an Article' do
   before do
+    @jon =  User.create(email: "jon@email.com", password: "password")
+    @fred = User.create(email: "fred@email.com", password: "password")
     @article = Article.create(title: 'This is 1st article', body: 'This is 1st body')
+  end
+
+  scenario "A non-signed in user dones not see Edit or Delete links" do
+    visit '/'
+
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+
+    visit '/'
+
+    click_link @article.title
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A signed in owner sees both links" do
+    login_as(@jon)
+
+    visit '/'
+
+    click_link @article.title
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 
   scenario 'Display Single article' do
